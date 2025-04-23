7777e7e5937221991ea52dc54fb31c4f35d38e19
Viki Shi <vishi@ucsd.edu>
2025-04-22 20:27:13 -0700

    lab4

--- projects/projects.js
+++ projects/projects.js
@@ -2,9 +2,15 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const projects = await fetchJSON('../lib/projects.json');
+  console.log('Loaded projects:', projects);
+
   const container = document.querySelector('.projects');
   const title = document.querySelector('.projects-title');
 
-  renderProjects(projects, container, 'h2');
-  if (title) title.textContent = projects.length;
+  if (projects && projects.length > 0) {
+    renderProjects(projects, container, 'h2');
+    if (title) title.textContent = projects.length;
+  } else {
+    container.innerHTML = '<p>No projects found.</p>';
+  }
 });
