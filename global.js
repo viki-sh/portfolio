dbb4c78b4152bbc7cc47be5ba4bd858bb603f273
Viki Shi <vishi@ucsd.edu>
2025-04-22 20:53:10 -0700

    lab4

--- global.js
+++ global.js
@@ -92,3 +92,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
