caa5a42d7ca9aec88cccb69b525ccd22920b25fb
Viki Shi <vishi@ucsd.edu>
2025-04-28 22:32:44 -0700

    l5

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const projects = await fetchJSON('../lib/projects.json');
@@ -14,6 +15,21 @@ document.addEventListener('DOMContentLoaded', async () => {
   } else {
     listContainer.innerHTML = '<p>No projects found.</p>';
   }
+
+  // D3 code to draw full circle
+  const arcGenerator = d3.arc()
+    .innerRadius(0)
+    .outerRadius(50);
+
+  const arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+  d3.select('#projects-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', 'red');
 });
 
 
