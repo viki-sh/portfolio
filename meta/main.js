f7ec5dbc967e471f78f0e5e1dc10a3f66bf8d089
Viki Shi <vishi@ucsd.edu>
2025-05-08 23:58:47 -0700

    a

--- meta/main.js
+++ meta/main.js
@@ -1,5 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+// Step 1.1: Load and parse CSV
 async function loadData() {
   const data = await d3.csv('/portfolio/meta/loc.csv', (row) => ({
     ...row,
@@ -9,13 +10,17 @@ async function loadData() {
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
+
   return data;
 }
 
+// Step 1.2: Process commit information
 function processCommits(data) {
   return d3.groups(data, d => d.commit).map(([commit, lines]) => {
-    const { author, date, time, timezone, datetime } = lines[0];
-    const result = {
+    let first = lines[0];
+    let { author, date, time, timezone, datetime } = first;
+
+    let ret = {
       id: commit,
       url: 'https://github.com/viki-sh/portfolio/commit/' + commit,
       author,
@@ -26,36 +31,56 @@ function processCommits(data) {
       hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
       totalLines: lines.length,
     };
-    Object.defineProperty(result, 'lines', {
+
+    Object.defineProperty(ret, 'lines', {
       value: lines,
       writable: false,
       enumerable: false,
       configurable: true
     });
-    return result;
+
+    return ret;
   });
 }
 
+// Step 1.3: Render summary stats
 function renderCommitInfo(data, commits) {
   d3.select('#stats').append('h2').text('Summary');
+
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
-  dl.append('dt').text('Commits'); dl.append('dd').text(commits.length);
-  dl.append('dt').text('Files'); dl.append('dd').text(d3.group(data, d => d.file).size);
-  dl.append('dt').text('Max depth'); dl.append('dd').text(d3.max(data, d => d.depth));
-  dl.append('dt').text('Longest line'); dl.append('dd').text(d3.max(data, d => d.length));
+
+  dl.append('dt').text('Commits');
+  dl.append('dd').text(commits.length);
+
+  const numFiles = d3.group(data, d => d.file).size;
+  dl.append('dt').text('Files');
+  dl.append('dd').text(numFiles);
+
+  const maxDepth = d3.max(data, d => d.depth);
+  dl.append('dt').text('Max depth');
+  dl.append('dd').text(maxDepth);
+
+  const longestLine = d3.max(data, d => d.length);
+  dl.append('dt').text('Longest line');
+  dl.append('dd').text(longestLine);
+
+  const fileLineCounts = d3.rollups(data, v => v.length, d => d.file);
+  const maxLines = d3.max(fileLineCounts, d => d[1]);
   dl.append('dt').text('Max lines');
-  dl.append('dd').text(d3.max(d3.rollups(data, v => v.length, d => d.file), d => d[1]));
+  dl.append('dd').text(maxLines);
 }
 
+// Step 2: Render scatterplot of time/day of commits
 function renderScatterPlot(data, commits) {
-  const margin = { top: 30, right: 20, bottom: 40, left: 60 };
+  const margin = { top: 20, right: 30, bottom: 50, left: 60 };
   const width = 1000 - margin.left - margin.right;
-  const height = 500 - margin.top - margin.bottom;
+  const height = 600 - margin.top - margin.bottom;
 
-  const svg = d3.select('#chart').append('svg')
+  const svg = d3.select('#chart')
+    .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
     .append('g')
@@ -70,7 +95,19 @@ function renderScatterPlot(data, commits) {
     .domain([0, 24])
     .range([height, 0]);
 
-  svg.append('g').selectAll('circle')
+  // Gridlines (horizontal)
+  svg.append('g')
+    .attr('class', 'gridlines')
+    .call(
+      d3.axisLeft(yScale)
+        .tickFormat('')
+        .tickSize(-width)
+    );
+
+  // Draw circles
+  svg.append('g')
+    .attr('class', 'dots')
+    .selectAll('circle')
     .data(commits)
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
@@ -78,14 +115,29 @@ function renderScatterPlot(data, commits) {
     .attr('r', 5)
     .attr('fill', 'steelblue');
 
+  // X Axis
   svg.append('g')
     .attr('transform', `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale).ticks(6).tickFormat(d3.timeFormat("%b %d")));
+    .call(d3.axisBottom(xScale).ticks(6).tickFormat(d3.timeFormat('%b %d')))
+    .append('text')
+    .attr('x', width)
+    .attr('y', 35)
+    .attr('fill', 'black')
+    .attr('text-anchor', 'end')
+    .text('Date');
 
+  // Y Axis
   svg.append('g')
-    .call(d3.axisLeft(yScale).ticks(8).tickFormat(d => String(d % 24).padStart(2, '0') + ':00'));
+    .call(d3.axisLeft(yScale).ticks(8).tickFormat(d => String(d % 24).padStart(2, '0') + ':00'))
+    .append('text')
+    .attr('x', -40)
+    .attr('y', -10)
+    .attr('fill', 'black')
+    .attr('text-anchor', 'start')
+    .text('Hour of Day');
 }
 
+// Load, process, and render
 let data = await loadData();
 let commits = processCommits(data);
 renderCommitInfo(data, commits);
