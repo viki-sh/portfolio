769a7348b2498164491bddd93be771de1bef9697
Viki Shi <vishi@ucsd.edu>
2025-05-08 23:51:37 -0700

    a

--- meta/main.js
+++ meta/main.js
@@ -16,33 +16,31 @@ async function loadData() {
 
 // Step 1.2: Process commit information
 function processCommits(data) {
-  return d3
-    .groups(data, d => d.commit)
-    .map(([commit, lines]) => {
-      let first = lines[0];
-      let { author, date, time, timezone, datetime } = first;
+  return d3.groups(data, d => d.commit).map(([commit, lines]) => {
+    let first = lines[0];
+    let { author, date, time, timezone, datetime } = first;
 
-      let ret = {
-        id: commit,
-        url: 'https://github.com/viki-sh/portfolio/commit/' + commit,
-        author,
-        date,
-        time,
-        timezone,
-        datetime,
-        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        totalLines: lines.length,
-      };
+    let ret = {
+      id: commit,
+      url: 'https://github.com/viki-sh/portfolio/commit/' + commit,
+      author,
+      date,
+      time,
+      timezone,
+      datetime,
+      hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+      totalLines: lines.length,
+    };
 
-      Object.defineProperty(ret, 'lines', {
-        value: lines,
-        writable: false,
-        enumerable: false,
-        configurable: true
-      });
-
-      return ret;
+    Object.defineProperty(ret, 'lines', {
+      value: lines,
+      writable: false,
+      enumerable: false,
+      configurable: true
     });
+
+    return ret;
+  });
 }
 
 // Step 1.3: Render summary stats
@@ -73,19 +71,20 @@ function renderCommitInfo(data, commits) {
   const maxLines = d3.max(fileLineCounts, d => d[1]);
   dl.append('dt').text('Max lines');
   dl.append('dd').text(maxLines);
-
 }
 
 // Step 2: Render scatterplot of time/day of commits
 function renderScatterPlot(data, commits) {
-  const width = 1000;
-  const height = 600;
+  const margin = { top: 20, right: 30, bottom: 50, left: 60 };
+  const width = 1000 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
 
-  const svg = d3
-    .select('#chart')
+  const svg = d3.select('#chart')
     .append('svg')
-    .attr('viewBox', `0 0 ${width} ${height}`)
-    .style('overflow', 'visible');
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
   const xScale = d3.scaleTime()
     .domain(d3.extent(commits, d => d.datetime))
@@ -96,9 +95,9 @@ function renderScatterPlot(data, commits) {
     .domain([0, 24])
     .range([height, 0]);
 
-  const dots = svg.append('g').attr('class', 'dots');
-
-  dots.selectAll('circle')
+  svg.append('g')
+    .attr('class', 'dots')
+    .selectAll('circle')
     .data(commits)
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
@@ -106,28 +105,29 @@ function renderScatterPlot(data, commits) {
     .attr('r', 5)
     .attr('fill', 'steelblue');
 
-  // Add axes
-  const xAxis = d3.axisBottom(xScale).ticks(6).tickFormat(d3.timeFormat("%b %d"));
-  const yAxis = d3.axisLeft(yScale).ticks(6);
-
+  // X Axis
   svg.append('g')
     .attr('transform', `translate(0, ${height})`)
-    .call(xAxis)
-    .call(g => g.append('text')
-      .attr('x', width)
-      .attr('y', -6)
-      .attr('fill', 'black')
-      .attr('text-anchor', 'end')
-      .text('Date'));
+    .call(d3.axisBottom(xScale).ticks(6).tickFormat(d3.timeFormat("%b %d")))
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', 40)
+    .attr('fill', 'black')
+    .attr('text-anchor', 'middle')
+    .attr('font-size', '1rem')
+    .text('Date');
 
+  // Y Axis
   svg.append('g')
-    .call(yAxis)
-    .call(g => g.append('text')
-      .attr('x', 6)
-      .attr('y', 6)
-      .attr('fill', 'black')
-      .attr('text-anchor', 'start')
-      .text('Hour of Day'));
+    .call(d3.axisLeft(yScale).ticks(6))
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('x', -height / 2)
+    .attr('y', -45)
+    .attr('fill', 'black')
+    .attr('text-anchor', 'middle')
+    .attr('font-size', '1rem')
+    .text('Hour of Day');
 }
 
 // Load, process, and render
