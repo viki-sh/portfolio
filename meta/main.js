f970c57921952a97d699bb4226320596b195a6c5
Viki Shi <vishi@ucsd.edu>
2025-05-08 22:58:18 -0700

    a

--- meta/main.js
+++ meta/main.js
@@ -1,23 +1,9 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-console.log("✅ main.js is running");
-
 async function loadData() {
-  try {
-    const data = await d3.csv('/portfolio/meta/loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line),
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-
-    console.log("✅ CSV loaded:", data);
-    return data;
-  } catch (error) {
-    console.error("❌ Failed to load CSV:", error);
-  }
+  const data = await d3.csv('loc.csv');
+  console.log(data);
+  return data;
 }
 
 let data = await loadData();
